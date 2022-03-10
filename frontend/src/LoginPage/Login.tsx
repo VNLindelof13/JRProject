ebe01164a3759ac953b1612d7eb897c78ae772fd
OmegaP1 <j.veloso.00@gmail.com>
2022-03-10 11:10:48 +0000

    LoginPage added

---
+++ frontend/src/LoginPage/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className='Login'>
+      <div className='InitialGrid'>
+        <img src='../' alt='' />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
