dc086b922b555cbea3ff9119b674b9bcb1700a95
miguelaguiar <miguel.aguiar.01@gmail.com>
2022-04-11 21:18:38 +0100

    React base

---
+++ jrproject/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div class="container">
+		<div class="image">
+			
+		</div>
+		<div>
+			
+		</div>
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
