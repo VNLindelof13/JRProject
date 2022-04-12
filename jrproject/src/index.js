c3acbf6a090021a34a015d3e0d3f26d85e4f2573
Bernardo Santos <bmrss@iscte-iul.pt>
2022-04-12 02:56:51 +0100

    Logo "centrado" com background

--- jrproject/src/index.js
+++ jrproject/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 ReactDOM.render(
   <React.StrictMode>
     <div class="justify-self-center">
-      <div class="mb-10"><img class="content-center" src="images/Seta.svg"></img></div>
+      <div class="mb-10 md:flex  max-w-[44%] mx-auto"><img class="content-center" src="images/Seta.svg"></img></div>
       <div class="grid grid-cols-3 gap-10 ml-52 mr-52 mb-32">
         <div class="h-full"><img src="images/Lisboa.svg"></img></div>
         <div class="h-full"><img src="images/Londres.svg"></img></div>
