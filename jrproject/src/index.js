72b86a8b110c8f1c369dabea2b454b15c4d1637f
CarlosFerreira13 <cdsfa@iscte-iul.pt>
2022-04-12 02:40:54 +0100

    Teste Lindi

--- jrproject/src/index.js
+++ jrproject/src/index.js
@@ -5,14 +5,14 @@ import "./index.css";
 ReactDOM.render(
   <React.StrictMode>
     <div class="justify-self-center">
-      <div class="mb-10 text-center"><img src="images/Seta.svg"></img></div>
-		<div class="grid grid-cols-3 gap-10 ml-52 mr-52 mb-32">
-      <div class="h-full"><img src="images/Lisboa.svg"></img></div>
-      <div class="h-full"><img src="images/Londres.svg"></img></div>
-      <div class="h-full"><img src="images/SaoFrancisco.svg"></img></div>
-    </div>
+      <div class="mb-10"><img class="content-center" src="images/Seta.svg"></img></div>
+      <div class="grid grid-cols-3 gap-10 ml-52 mr-52 mb-32">
+        <div class="h-full"><img src="images/Lisboa.svg"></img></div>
+        <div class="h-full"><img src="images/Londres.svg"></img></div>
+        <div class="h-full"><img src="images/SaoFrancisco.svg"></img></div>
+      </div>
 
-    <div class="text-center"><a><button>ENTRAR</button></a></div>
+      <div class="text-center"><a><button>ENTRAR</button></a></div>
     </div>
   </React.StrictMode>,
   document.getElementById("root")
